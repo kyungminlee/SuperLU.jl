a63dd14739ae0535d0fcddf4765266dd72799984
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-12-02 17:01:51 -0500

    Initial commit

---
+++ src/superlu_util.jl
@@ -0,0 +1,11 @@
+
+const superlu_options_fields = Dict(fieldname(superlu_options_t,i)=>(fieldoffset(superlu_options_t,i), fieldtype(superlu_options_t,i)) for i = 1:fieldcount(superlu_options_t))
+function set_options(options::superlu_options_t, field::Symbol, value)
+    options_ref = Ref(options)
+    options_ptr = Base.unsafe_convert(Ptr{superlu_options_t}, options_ref)
+    offset, type = superlu_options_fields[field]
+    GC.@preserve options_ref begin
+        Base.unsafe_store!(Ptr{type}(options_ptr + offset), value)
+        return options_ref[]
+    end
+end
