5f8bae8756238d2838f8423034b709417160b856
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-12-02 23:02:43 -0500

    Fix namespace for \

--- src/linalg.jl
+++ src/linalg.jl
@@ -15,7 +15,7 @@ function LinearAlgebra.ldiv!(a::Adjoint{Tv, LUDecomposition{Tv, SuperLUInt}}, b:
 end
 
 # not sure why Julia only implements \ for NoTrans and Adjoint.
-function \(tF::Transpose{Tv,LUDecomposition{Tv, Ti}}, B::AbstractVecOrMat) where {Tv, Ti}
+function Base.:(\)(tF::Transpose{Tv,LUDecomposition{Tv, Ti}}, B::AbstractVecOrMat) where {Tv, Ti}
     require_one_based_indexing(B)
     F = tF.parent
     TFB = typeof(oneunit(eltype(B)) / oneunit(eltype(F)))
